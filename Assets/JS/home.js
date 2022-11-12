34e52d205ccb452b2170686c0bd4250f154d2559
magmesser <magmesser@gmail.com>
2022-11-12 15:03:33 -0500

    added color palette to background, text, and buttons

--- Assets/JS/home.js
+++ Assets/JS/home.js
@@ -2,36 +2,34 @@ var recipeInput = $("#recipe-input");
 var movieInput = $("#movie-input");
 var submitBtn = $("#submit-btn");
 
-
-
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.modal');
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".modal");
   M.Modal.init(elems);
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('select');
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll("select");
   M.FormSelect.init(elems);
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-  var auto = document.querySelectorAll('.autocomplete');
+document.addEventListener("DOMContentLoaded", function () {
+  var auto = document.querySelectorAll(".autocomplete");
   M.Autocomplete.init(auto, {
     data: {
-      'Chicken': null,
-      'Pasta': null,
-      'Pizza': null,
-      'Salad': null
+      Chicken: null,
+      Pasta: null,
+      Pizza: null,
+      Salad: null,
     },
-    limit: 1
-  })
+    limit: 1,
+  });
 });
 
 function formSubmit(event) {
   event.preventDefault();
-  var queryUrl = "./results.html?q=" + recipeInput.val() + "&movie=" + movieInput.val();
+  var queryUrl =
+    "./results.html?q=" + recipeInput.val() + "&movie=" + movieInput.val();
   location.assign(queryUrl);
 }
 
 submitBtn.click(formSubmit);
-
--- Assets/JS/results.js
+++ Assets/JS/results.js
@@ -1,7 +1,7 @@
 var movieApiKey = "k_ctt698mf";
 var recipeImg = $("#recipe-img");
 var recipeUrl = $("#recipe-url");
-var ingredientsList = $("#ingredients-list")
+var ingredientsList = $("#ingredients-list");
 var movieContainer = $("#movieContainer");
 var dontLikeMovie = $("#dont-like-movie");
 var dontLikeRecipe = $("#dont-like-recipe");
@@ -12,8 +12,8 @@ var submitBtn = $("#submit-btn");
 
 function grabParams() {
   var apiParamsArr = document.location.search.split("&");
-  var recipe = apiParamsArr[0].split("=").pop()
-  var movie = apiParamsArr[1].split("=").pop()
+  var recipe = apiParamsArr[0].split("=").pop();
+  var movie = apiParamsArr[1].split("=").pop();
   recipeAPI(recipe);
   getMovies(movie);
 }
@@ -44,24 +44,28 @@ var getMovies = function (movieGenre) {
         plot: data.results[i].plot,
       };
       console.log(movieData);
-    })
+    });
 };
 
 function recipeAPI(recipe) {
-  var recipeQueryUrl = "https://api.edamam.com/api/recipes/v2?type=public&q=" + recipe + "&app_id=ffb9b7d6&app_key=177ebaaec33bbf5decf819cd890a664a"
+  var recipeQueryUrl =
+    "https://api.edamam.com/api/recipes/v2?type=public&q=" +
+    recipe +
+    "&app_id=ffb9b7d6&app_key=177ebaaec33bbf5decf819cd890a664a";
   fetch(recipeQueryUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      updateRecipeCard(data)
-    })
+      updateRecipeCard(data);
+    });
 }
 
 function updateRecipeCard(values) {
-  var randomRecipe = values.hits[Math.floor(Math.random() * values.hits.length)].recipe;
-  var ingredients = randomRecipe.ingredientLines
-  recipeImg.attr("src", randomRecipe.image)
+  var randomRecipe =
+    values.hits[Math.floor(Math.random() * values.hits.length)].recipe;
+  var ingredients = randomRecipe.ingredientLines;
+  recipeImg.attr("src", randomRecipe.image);
   recipeUrl.attr("href", randomRecipe.url);
   console.log(randomRecipe);
   for (let i = 0; i < ingredients.length; i++) {
@@ -71,66 +75,66 @@ function updateRecipeCard(values) {
 }
 
 function displayMovieSearch(movieData) {
-  var randomMovie = movieData.results[Math.floor(Math.random() * movieData.results.length)]
+  var randomMovie =
+    movieData.results[Math.floor(Math.random() * movieData.results.length)];
 
   var movieTitleEl = $("#title").text(randomMovie.title);
   movieContainer.append(movieTitleEl);
 
-  var movieRatingEl = $('#rating').text(randomMovie.contentRating);
+  var movieRatingEl = $("#rating").text(randomMovie.contentRating);
   movieContainer.append(movieRatingEl);
 
-  var moviePlotEl = $('#plot').text(randomMovie.plot);
+  var moviePlotEl = $("#plot").text(randomMovie.plot);
   movieContainer.append(moviePlotEl);
 
   var movieImgEl = $("#poster").attr("src", randomMovie.image);
   movieContainer.append(movieImgEl);
-
 }
 
 grabParams();
 
-
-dontLikeRecipe.click(function() {
+dontLikeRecipe.click(function () {
   var apiParamsArr = document.location.search.split("&");
   var recipe = apiParamsArr[0].split("=").pop();
   ingredientsList.empty();
   recipeAPI(recipe);
-})
+});
 
 dontLikeMovie.click(function () {
   var apiParamsArr = document.location.search.split("&");
-  var movie = apiParamsArr[1].split("=").pop()
+  var movie = apiParamsArr[1].split("=").pop();
   getMovies(movie);
-})
+});
 
 // copy of Modal from home page
 
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.modal');
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".modal");
   M.Modal.init(elems);
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('select');
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll("select");
   M.FormSelect.init(elems);
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-  var auto = document.querySelectorAll('.autocomplete');
+document.addEventListener("DOMContentLoaded", function () {
+  var auto = document.querySelectorAll(".autocomplete");
   M.Autocomplete.init(auto, {
     data: {
-      'Chicken': null,
-      'Pasta': null,
-      'Pizza': null,
-      'Salad': null
+      Chicken: null,
+      Pasta: null,
+      Pizza: null,
+      Salad: null,
     },
-    limit: 1
-  })
+    limit: 1,
+  });
 });
 
 function formSubmit(event) {
   event.preventDefault();
-  var queryUrl = "./results.html?q=" + recipeInput.val() + "&movie=" + movieInput.val();
+  var queryUrl =
+    "./results.html?q=" + recipeInput.val() + "&movie=" + movieInput.val();
   location.assign(queryUrl);
   grabParams();
 }
