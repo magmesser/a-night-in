773b4ca1822efec27e74eb58daf68603d5f85165
Albert <alberto.suarez8@outlook.com>
2022-11-13 20:27:20 -0500

    Added a choose for me button in the modal with randomizer functionality.

--- Assets/JS/home.js
+++ Assets/JS/home.js
@@ -1,6 +1,7 @@
 var recipeInput = $("#recipe-input");
 var movieInput = $("#movie-input");
 var submitBtn = $("#submit-btn");
+var randomBtn = $("#random-btn");
 
 document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".modal");
@@ -32,4 +33,14 @@ function formSubmit(event) {
   location.assign(queryUrl);
 }
 
+function randomInputs(event) {
+  var movieGenre = "action&animation&biography&comedy&crime&documentary&drama&fantasy&horror&musical&romance&sci-fi&thriller".split("&")
+  var foodInput = "chicken&steak&salad&pasta&fish&seafood&beef&lamb&veal&pork&eggs&tacos&pizza".split("&")
+  event.preventDefault();
+  var queryUrl =
+    "./results.html?q=" + foodInput[Math.floor(Math.random()*foodInput.length)] + "&movie=" + movieGenre[Math.floor(Math.random()*movieGenre.length)];
+  location.assign(queryUrl);
+}
+
 submitBtn.click(formSubmit);
+randomBtn.click(randomInputs);
